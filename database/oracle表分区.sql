1aa78950f091f2d193c925f647bcc3f4cf528a48
wuchengqian <chengqian.wu@wasion.com>
2019-06-24 18:46:00 +0800

    add delete partition

--- database/oracle表分区.sql
+++ database/oracle表分区.sql
@@ -5,4 +5,8 @@ SELECT partitioned FROM ALL_TABLES WHERE TABLE_NAME = 'RD_ELEC_CODE';
 select * from RD_ELEC_CODE partition(RD_ELEC_CODE99981212);
 
 -- create partition
-alter table RD_ELEC_CODE add partition RD_ELEC_CODE201905 VALUES LESS THAN (1559318400);  1556640000
+alter table RD_ELEC_CODE add partition RD_ELEC_CODE201905 VALUES LESS THAN (1559318400);
+alter table RD_ELEC_CODE add partition RD_ELEC_CODE99981212 VALUES LESS THAN (MAXVALUE);
+
+-- delete partition
+alter table RD_ELEC_CODE drop partition RD_ELEC_CODE99981212;
